141c93959d2102c44e5e41a634c4db87698d9409
AndreyPokotilo <dpon444@gmail.com>
2025-11-08 17:17:17 +0100

    add page animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,10 +13,10 @@ import {
 } from "../../pages";
 
  const pageAnimation = {
-  initial: { opacity: 0, y: -10 },
+  initial: { opacity: 0, y: 0 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 10 },
-  transition: { duration: 0.1, ease: "easeInOut" },
+  exit: { opacity: 0, y: 0 },
+  transition: { duration: 0,  },
 };
 
 function App() {
