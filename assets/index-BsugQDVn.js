2953c75fbb15e53bd105534d2a6ea04e30b8b8d6
AndreyPokotilo <AndreyPokotilo@users.noreply.github.com>
2025-11-11 21:01:58 +0000

    deploy: e790e99229665346c1ed3e8e5a1311f3d48f4178

--- assets/index-BsugQDVn.js
+++ assets/index-BsugQDVn.js
@@ -773,7 +773,7 @@ cursor: pointer;
 
     @media (min-width: ${({theme:n})=>n.breakpoints.s}) and (max-width: 1023px) {
       width: 30vw;
-;
+      top: 95px;
   }
   };`,Nx=({mobMenuIsOpen:n})=>v.jsxs(Bx,{mobMenuIsOpen:n,children:[v.jsx(w4,{}),v.jsxs(O4,{children:[v.jsx("span",{children:"UA"}),v.jsx(C4,{})]})]}),Ux=()=>v.jsx("svg",{version:"1.1",id:"Layer_1",xmlns:"http://www.w3.org/2000/svg",x:"0px",y:"0px",width:"42",height:"42",viewBox:"0 0 122.88 95.95",children:v.jsx("path",{d:"M8.94,0h105c4.92,0,8.94,4.02,8.94,8.94l0,0c0,4.92-4.02,8.94-8.94,8.94h-105C4.02,17.88,0,13.86,0,8.94l0,0 C0,4.02,4.02,0,8.94,0L8.94,0z M8.94,78.07h105c4.92,0,8.94,4.02,8.94,8.94l0,0c0,4.92-4.02,8.94-8.94,8.94h-105 C4.02,95.95,0,91.93,0,87.01l0,0C0,82.09,4.02,78.07,8.94,78.07L8.94,78.07z M8.94,39.03h105c4.92,0,8.94,4.02,8.94,8.94l0,0 c0,4.92-4.02,8.94-8.94,8.94h-105C4.02,56.91,0,52.89,0,47.97l0,0C0,43.06,4.02,39.03,8.94,39.03L8.94,39.03z"})});function Hx(){const[n,a]=M.useState(!1),s=j4(),r=()=>{a(!n)};return v.jsx(jx,{children:v.jsx(wx,{children:v.jsxs(Ox,{children:[v.jsxs(Lx,{to:"/",end:!0,children:[v.jsx(Dx,{children:v.jsx(zx,{})}),v.jsxs(Rx,{children:["BEREHYNJA",v.jsx("span",{children:"український простір"})]})]}),s>=1024?v.jsxs(v.Fragment,{children:[v.jsx(w4,{}),v.jsxs(O4,{children:[v.jsx("span",{children:"UA"}),v.jsx(C4,{})]})]}):v.jsxs(v.Fragment,{children:[v.jsx(Vx,{onClick:r,children:v.jsx(Ux,{})}),v.jsx(Nx,{mobMenuIsOpen:n})]})]})})})}const qx=ut.footer`
   background-color: #161942ff;
