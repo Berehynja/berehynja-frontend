3deb7cf778aaff491ec30ed7fe89bd375a05cc29
AndreyPokotilo <dpon444@gmail.com>
2025-11-24 18:08:02 +0100

    create language switcher component

--- src/components/LanguageSwitcher/LanguageSwitcher.styled.ts
+++ src/components/LanguageSwitcher/LanguageSwitcher.styled.ts
@@ -5,14 +5,14 @@ export const LangugeBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 60px;
+  width: 70px;
   height: 30px;
   margin-left: auto;
   margin-right: auto;
   background-color: white;
   border: none;
   cursor: pointer;
-  & svg {
+  /* & svg {
     margin-left: 5px;
     fill: #ffffffff;
     transition: 250ms;
@@ -20,7 +20,7 @@ export const LangugeBox = styled.div`
   &:hover svg {
     stroke: #1973faff;
     fill: #fffcceff;
-  }
+  } */
   @media (max-width: 1023px) {
     margin-bottom: 20px;
   }
--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,25 +1,23 @@
-import ReactCountryFlag from 'react-country-flag';
+import ReactCountryFlag from "react-country-flag";
 import { LangugeIcon } from "../icons/LangugeIcon";
 import { LangugeBox } from "./LanguageSwitcher.styled";
 
 export const LanguageSwitcher = () => {
   return (
     <LangugeBox>
-        <span>
-              <ReactCountryFlag
-                countryCode="ua"
-                svg
-                style={{
-                  width: '20px',
-                  height: '14px',
-                  borderRadius: '2px',
-                  marginRight: '15px'
-                }}
-                title="UA"
-              />
-            </span>
-      <span>UA</span>
       <LangugeIcon />
+      <ReactCountryFlag
+        countryCode="ua"
+        svg
+        style={{
+          width: "20px",
+          height: "14px",
+          borderRadius: "2px",
+          marginRight: "15px",
+        }}
+        title="UA"
+      />
+      <span>UA</span>
     </LangugeBox>
   );
 };
