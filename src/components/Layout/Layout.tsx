32f3b657cddd4c9f0f03ebc00b7c3d4c390f84a0
AndreyPokotilo <dpon444@gmail.com>
2025-11-27 09:55:18 +0100

    add component skroll to top

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import {  Container, Main, LayoutContainer } from "./Layout.styled";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import { ScrollToTop } from "../ScrollToTop/ScrollToTop";
 
 export const Layout = () => {
   return (
     <LayoutContainer>
+      <ScrollToTop />
       <Header />
         <Container>
           <Main>
