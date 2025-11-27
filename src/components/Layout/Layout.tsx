44515be2f13e374b6bc369481060ef336df2aae0
AndreyPokotilo <dpon444@gmail.com>
2025-11-27 10:06:08 +0100

    add initialisation of language to pathname

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,23 @@
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import {  Container, Main, LayoutContainer } from "./Layout.styled";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { ScrollToTop } from "../ScrollToTop/ScrollToTop";
+import { useTranslation } from "react-i18next";
 
 export const Layout = () => {
+  const { i18n } = useTranslation();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams();
+    if (searchParams.get("lng") !== i18n.language) {
+      searchParams.set("lng", i18n.language);
+      navigate({ pathname: location.pathname, search: searchParams.toString() }, { replace: true });
+    }
+  }, [location.pathname, i18n.language, navigate]);
   return (
     <LayoutContainer>
       <ScrollToTop />
