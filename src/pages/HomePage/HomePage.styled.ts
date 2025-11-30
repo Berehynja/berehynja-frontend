4fb874323d7b8b2bd751efaa4cbe914aba40262f
AndreyPokotilo <dpon444@gmail.com>
2025-11-30 16:57:13 +0100

    add text to banner

--- src/pages/HomePage/HomePage.styled.ts
+++ src/pages/HomePage/HomePage.styled.ts
@@ -8,7 +8,11 @@ width: 100%;
 export const Baner = styled.div`
   min-width: 320px;
   min-height: 300px;
- 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: start; 
+  padding: 40px; 
   background-image: url(${ban});
   background-repeat: no-repeat;
   background-size: cover;
@@ -28,6 +32,30 @@ export const Baner = styled.div`
     min-height: 850px;
   }
 `;
+
+export const BannerTitele = styled.h1`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+  color: #ffffffff;
+  text-align: start;
+  margin-bottom: 20px;
+`;
+
+export const BanerText = styled.p`
+  display: block;
+  width: 100%; 
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  text-indent: 30px;
+  color: #ffffffff;
+  margin: 0 auto;
+  padding: 20px;
+  font-size: 28px;
+  line-height: 1.5;
+  `;
 
 export const HomeContainer = styled.div`
   width: 100%;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,39 +1,50 @@
-import { Baner, Hero, HomeContainer } from "./HomePage.styled";
+import { Baner, BanerText, BannerTitele, Hero, HomeContainer } from "./HomePage.styled";
 
 export function HomePage() {
   return (
     <>
-    <Hero>
-       <Baner/>
-    </Hero>
-    <HomeContainer>
-      
-      <p>HOME</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-    </HomeContainer>
+      <Hero>
+        <Baner>
+          <BannerTitele>Ласкаво просимо до «Berehynja»!</BannerTitele>
+          <BanerText>
+                «Berehynja» — це український простір у Німеччині, створений для
+            дітей, підлітків та дорослих, які вимушено опинилися далеко від
+            дому. Ми об’єднуємо українські родини, допомагаємо зберігати мову,
+            культуру та традиції, а також підтримуємо тих, хто проходить шлях
+            адаптації та інтеграції в нове середовище. <br /> 
+            {/* Наша мета — створити тепле та дружнє середовище, де кожен
+            почуватиметься як вдома, знайде нових друзів та отримає необхідну
+            підтримку. */}
+          </BanerText>
+        </Baner>
+      </Hero>
+      <HomeContainer>
+        <p>HOME</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+          aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+          natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+          dolorum adipisci, minus vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+          aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+          natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+          dolorum adipisci, minus vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+          aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+          natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+          dolorum adipisci, minus vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+          aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+          natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+          dolorum adipisci, minus vel.
+        </p>
+      </HomeContainer>
     </>
   );
 }
