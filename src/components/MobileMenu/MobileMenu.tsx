72044575d1888f8c24fc5f77a2e0e27288b1c95d
AndreyPokotilo <dpon444@gmail.com>
2025-12-09 14:09:44 +0100

    add new background to mobmenu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import { X } from 'lucide-react';
-import dekor from "../../images/dek.jpg";
+// import dekor from "../../images/dek.jpg";
 import { HeaderNav } from "../HeaderNav/HeaderNav";
 import { LanguageSwitcher } from "../LanguageSwitcher/LanguageSwitcher";
 import { useEffect } from 'react';
@@ -42,10 +42,10 @@ export const MobileMenu = ({ mobMenuIsOpen, setMobMenuIsOpen }: MobileMenuProps)
       <button className=" absolute top-4 right-4 z-10 " onClick={()=>setMobMenuIsOpen(!mobMenuIsOpen)} aria-label='close mobile menu'>
         <X className=' stroke-stone-600 ' size={32}/>
       </button>
-      <div 
+      {/* <div 
       className=" flex flex-col bg-repeat-y bg-size-[1.5rem] w-6 h-full"
       style={{backgroundImage: `url(${dekor})`}}
-      ></div>
+      ></div> */}
 
       <div className=" relative flex flex-col justify-start items-center w-full mx-auto py-16 ">
        <HeaderNav />
