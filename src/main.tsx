532906e6aa797871e9ff84d1dbc1dad0534d1ca9
AndreyPokotilo <dpon444@gmail.com>
2025-12-15 20:39:48 +0100

    delet compounets glabal style and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+
 import "./i18n.tsx";
 import "modern-normalize/modern-normalize.css";
+import "./index.css";
 
 import App from "./components/App/App.tsx";
 
