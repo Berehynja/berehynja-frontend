5e1e753d048bf60a4f40e3d1106209252eac5252
AndreyPokotilo <dpon444@gmail.com>
2025-12-18 12:11:42 +0100

    add title to page about us

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,127 +1,144 @@
-export const About = ()  => {
+export const About = () => {
   return (
     <div className="w-full ">
+      <div className="flex justify-center items-center gap-8 py-12 font-montserratBold">
+        <div className="max-w-4xl ">
+          <h1 className="flex  text-5xl  font-bold py-4 justify-center ">
+            Про Berehynja{" "}
+          </h1>{" "}
+          <div className="w-24 h-1 bg-linear-to-r from-blue-500 to-yellow-400 mx-auto mb-4"></div>
+        </div>
+
+        <p className="flex justify-center items-center max-w-3xl text-xl leading-8">
+          Громадська організація, присвячена підтримці українських біженців у
+          їхній подорожі адаптації та інтеграції в Німеччині.
+        </p>
+      </div>
+
       <p>Історія команди</p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
+        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
+        dolorum adipisci, minus vel.
+      </p>
       <p>Статут PDF</p>
       <p>Партнерство</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p><p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p><p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
     </div>
   );
-}
+};
