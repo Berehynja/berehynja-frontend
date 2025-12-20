1a22468e25657076f1185827ccf409a888bdd956
AndreyPokotilo <dpon444@gmail.com>
2025-12-20 18:54:45 +0100

    add sections teams and partners to page abou us

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,4 +1,6 @@
 import about from "../../images/bereg-about22.jpg";
+import avatar from "../../images/icons8-avatar-3d-fluency/icons8-avatar-100.png";
+import partner from "../../images/icons8-penguin-color/icons8-penguin-48.png";
 console.log("🚀 ~ about:", about);
 
 export const About = () => {
@@ -23,7 +25,9 @@ export const About = () => {
 
       <section className="flex w-full flex-col md:flex-row justify-center items-center md:gap-6 lg:gap-8 py-12 ">
         <div className="min-w-80 max-w-120 md:max-w-1/2 h-130  px-6  rounded-lg shadow-lg">
-          <h2 className="font-montserratBold text-2xl py-3 xl:py-4">Історія команди</h2>
+          <h2 className="font-montserratBold text-2xl py-3 xl:py-4">
+            Історія команди
+          </h2>
           <p className="font-montserratRegular text-base lg:text-lg leading-5 md:leading-5 lg:leading-5.5 xl:leading-7 xl:py-5">
             {" "}
             BEREHYNJA була заснована у відповідь на зростаючу потребу в
@@ -48,39 +52,81 @@ export const About = () => {
         </div>
       </section>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
-        aspernatur cupiditate voluptatibus! Odio suscipit tempora pariatur
-        natus, culpa ipsum vero cupiditate assumenda quaerat placeat, enim
-        dolorum adipisci, minus vel.
-      </p>
+      <section className="flex flex-col justify-center items-center py-10">
+        <div className="max-w-full mb-10">
+          <h2 className="flex flex-col justify-center items-center font-montserratBold text-2xl py-3 xl:py-4">
+            Наша команда
+          </h2>
+          <ul className="max-w-120 md:max-w-full grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 list-none text-base lg:text-lg leading-5 md:leading-5 lg:leading-5.5 xl:leading-7 xl:py-5">
+            <li className="flex rounded-b-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Олена Іваненко <br /> Засновниця та виконавча директорка</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Марія Ковальчук <br /> Координаторка програм</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Ірина Петренко <br /> Спеціалістка з комунікацій</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Анна Шевченко <br /> Юридична консультантка</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Оксана Литвин <br /> Координаторка волонтерів</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Світлана Бондаренко <br /> Фінансовий менеджер</p>
+            </li>
+            <li className="flex rounded-sm shadow-card p-3">
+              <img src={avatar} />
+              <p>Тетяна Грищук <br /> Культурний куратор</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="max-w-full">
+          <h2 className="flex flex-col justify-center items-center font-montserratBold text-2xl py-3 xl:py-4">
+            Наші партнери
+          </h2>
+          <ul className="max-w-120 md:max-w-full grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 list-none text-base lg:text-lg leading-5 md:leading-5 lg:leading-5.5 xl:leading-7 xl:py-5">
+            <li className="flex rounded-b-sm bg-amber-50 shadow-card p-3">
+              <img src={partner} alt="p"/>
+              <p>Генеральне консульство України в Німеччині</p>
+            </li>
+            <li className="flex rounded-sm bg-blue-50 shadow-card p-3">
+              <img src={partner} alt="p"/>
+              <p>
+                Міністерство закордонних справ України (проект «Український
+                інститут»)
+              </p>
+            </li>
+            <li className="flex rounded-sm bg-emerald-50 shadow-card p-3">
+              <img src={partner} alt="p"/>
+              <p>Integrationamt Bad Oeynhausen</p>
+            </li>
+            <li className="flex rounded-sm bg-fuchsia-50 shadow-card p-3">
+              <img src={partner}  alt="p"/>
+              <p>Plast – Ukrainischer Pfadfinderbund в Німеччин</p>
+            </li>
+            <li className="flex rounded-sm bg-indigo-50 shadow-card p-3">
+              <img src={partner} alt="p"/>
+              <p>Українська суботня школа в Гамбурзі</p>
+            </li>
+            <li className="flex rounded-sm bg-pink-50 shadow-card p-3">
+              <img src={partner} alt="p"/>
+              <p>Міжнародний фонд «Відродження»</p>
+            </li>
+          </ul>
+        </div>
+      </section>
 
       <p>Статут PDF</p>
-      <p>Партнерство</p>
+      
     </div>
   );
 };
