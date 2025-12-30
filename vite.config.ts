dce908cc249e471b24e424d09b8df2f254c1db53
AndreyPokotilo <dpon444@gmail.com>
2025-12-30 20:13:53 +0100

    fix styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/berehynja-frontend',
+  base: '/berehynja-frontend/',
   plugins: [react(),
     tailwindcss()],
 })
