150fe11fd3dbe633bc8777564489594e0839b661
AndreyPokotilo <dpon444@gmail.com>
2026-02-02 17:57:00 +0100

    add editing to event detail

--- src/components/AdminLogOut/BtnLogOut.tsx
+++ src/components/AdminLogOut/BtnLogOut.tsx
@@ -6,7 +6,7 @@ export default function AdminLogout() {
       onClick={logout}
       className="rounded bg-red-500 px-4 py-2 text-white transition hover:bg-red-600"
     >
-      Выйти
+      Log Out
     </button>
   );
 }
